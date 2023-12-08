c5aad8ff8ae3f998ce4f0a93fdaa74753ddb575d
Veronika Stojanovová <verca.stojanovova@gmail.com>
2023-12-08 09:57:21 +0000

    homepage

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("images");
     // Zkopírovat css/ to _site/css/
     eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("siema.min.js");
 
    
    return {
